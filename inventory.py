9ee3f26add59fa1cfbf4d4df0a0f603fd6b6b527
Owl-Code <paul.einwich@gmail.com>
2020-02-04 22:13:13 -0800

    Fix input

--- inventory.py
+++ inventory.py
@@ -12,19 +12,27 @@ PRODUCT_DATA = {1: 'ProductImportTemplate.csv',
                     3: 'StockImportTemplate.csv'}
 
 def select_data():
+    """
+        Ask user for a numerical input (1-3) and returns data set.
+
+        ARGS: 
+            None
+        RETURN:
+            df - Pandas data frame loaded from a csv
+    """
     while True:
         user_choice = input('1. Product\n2. Barcode\n3. Stock\n')
         if len(user_choice) > 1 and (user_choice < '1' and user_choice >= '3'):
             print("Input 1-3")
         else:
             if int(user_choice) == 1:
-                df = pd.read_csv(PRODUCT_DATA[user_choice])
+                df = pd.read_csv(PRODUCT_DATA[int(user_choice)])
                 return df
             elif int(user_choice) == 2:
-                df = pd.read_csv(PRODUCT_DATA[user_choice])
+                df = pd.read_csv(PRODUCT_DATA[int(user_choice)])
                 return df
             elif int(user_choice) == 3:
-                df = pd.read_csv(PRODUCT_DATA[user_choice])
+                df = pd.read_csv(PRODUCT_DATA[int(user_choice)])
                 return df
             else:
                 print('Something went wrong')
